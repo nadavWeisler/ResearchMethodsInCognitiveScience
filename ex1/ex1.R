921d0a310f9bd805a5e6b4621ce422f81e6385e2
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2022-04-23 22:09:26 +0300

    Finish ex1

--- ex1/ex1.R
+++ ex1/ex1.R
@@ -1,5 +1,5 @@
-install.packages("moments")
 library(moments)
+library(lsr)
 
 #Question 1
 #A
@@ -18,7 +18,7 @@ NormalDistributionByN = function(N, MEAN, SD, showHist=FALSE){
   return (sampleVector)
 }
 
-samples = NormalDistributionByN(N, MEAN, SD)
+samples = NormalDistributionByN(N, MEAN, SD, TRUE)
 print(mean(samples))
 print(sd(samples))
 
@@ -28,12 +28,13 @@ sds = c()
 allSamples = c()
 N_Values = c(3, 10, 50, 100, 250, 500)
 for (n in N_Values) {
-  current = NormalDistributionByN(n, MEAN, SD)
+  current = NormalDistributionByN(n, MEAN, SD,TRUE)
   allSamples = c(allSamples, current)
   means = c(means, mean(current))
   sds = c(sds, sd(current))
 }
-
+print(means)
+print(sds)
 #C
 plot(N_Values, means)
 abline(h = mean(allSamples), col = "darkgreen")
@@ -90,10 +91,14 @@ for (n in seq(50, 500, by=50)) {
 print(means)
 print(vars)
 
+getSkew = function(X, u, q){
+  return(mean(((X - u) / q) ^ 3))
+}
+
 #D
 plot(seq(50, 500, by=50), means, main = "Mean vs N value")
-abline(h = skewness(means), col = "darkgreen")
-print(skewness(means))
+abline(h = getSkew(X=0, u=MEAN, q=SD), col = "darkgreen")
+print(getSkew(X=0, u=MEAN, q=SD))
 
 #E
 plot(seq(50, 500, by=50), vars, main = "Var vs N value")
@@ -126,7 +131,8 @@ for (i in 1:10000) {
 hist(allValues, main = "Histograms of all t values")
 
 #D
-
+x = seq(-6, 6, length = 10000)
+curve(dt(x, df=N-1), from=-6, to=6)
 
 #Question 4
 
@@ -177,7 +183,48 @@ plot(SD_Values, allValues)
 
 #Question 6
 
+#Aנ
+setwd('C:/Repos/Study/ResearchMethodsInCognition/ex1')
+df = read.csv('neglect_data.csv')
+df = na.omit(df)
+colnames(df) = c("I", "Subject", "TrialNumber", "Length", "Location")
+
+#B
+df$LeftPart = df$Location / df$Length
+
+#C 
+ics = c()
+for(subject in c(5, 21, 13, 7)){
+  currentSubjectDf = df[df$Subject == subject,]
+  ics = c(ics, quantile(currentSubjectDf$LeftPart, probs = c(0.05 / 2, 1 - (0.05 / 2))))
+}
+print(ics)
+#D
+leftPartMean = c()
+for(subject in unique(df$Subject)){
+  currentSubjectDf = df[df$Subject == subject,]
+  leftPartMean = c(leftPartMean, mean(currentSubjectDf$LeftPart))
+}
+print(leftPartMean)
+
+#E
+quantile(leftPartMean, probs = c(0.05 / 2, 1 - (0.05 / 2)))
+
+
+#F
+tScore = getTDist(df$LeftPart, 0.55)
+
+#G
+pt(tScore, nrow(df) - 1, lower.tail = FALSE) 
+
+#H
+t.test(df$LeftPart, mu = 0.55)
+
+#I
+cohensD(df$LeftPart, mu = 0.55)
 
 
 
 
+
+  
